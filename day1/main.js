390e4c109857b3c9887f6dc325f4ddba06b21b2d
RiiXXD <thereforeme06@gmail.com>
2024-04-25 00:48:44 +0530

    day 1 basic intializing

---
+++ day1/main.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 2;
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(a, b);
---
+++ day1/main.ts
@@ -0,0 +1,6 @@
+const a=1;
+const b=2;
+function sum(a: number,b: number){
+     console.log(a+b);
+}
+sum(a,b);
